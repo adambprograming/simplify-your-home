630c041f6a770af01a713a963e714d5dd6545f8f
Adam <adambprograming@yahoo.com>
2023-11-21 23:46:40 +0100

    Initial

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+// Fonts
+import { Bebas_Neue, Baskervville } from 'next/font/google'
+// Styles
+import "./globals.scss";
+// Components
+
+
+const bebasNeue = Bebas_Neue({weight: '400', subsets: ['latin'], variable: '--font-bebasneue'})
+const baskervville = Baskervville({weight: '400', subsets: ['latin'], variable: '--font-baskervville'})
+
+export const metadata = {
+  title: 'SimplifyYourHome',
+  description: 'E-shop with gadgets to simlify your home',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${bebasNeue.variable} ${baskervville.variable}`}>
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+// Styles
+
+// Public & Assets
+
+// React/Next Functions
+
+// Context
+
+// Components
+
+export default function Home() {
+  return (
+    <main className="main">
+      <h1>Initial</h1>
+    </main>
+  );
+}
