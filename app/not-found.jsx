7ceb8f6f834d93da42466e91cd46d7ac69abfd5a
Adam <adambprograming@yahoo.com>
2023-11-24 03:30:19 +0100

    Make menu-dropdown reusable and added sublink option

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,12 +1,29 @@
 // Componenets
-import Btn from '@/components/r-btn-link/btn-link.component'
- 
+import Btn from "@/components/r-btn-link/btn-link.component";
+
 export default function NotFound() {
   return (
-    <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", WebkitTransform: "translate(-50%, -50%)"}}>
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        WebkitTransform: "translate(-50%, -50%)",
+      }}
+    >
       <h2>Not Found</h2>
-      <p style={{marginBottom: 20}}>Could not find requested content.</p>
-      <Btn content="Return Home" itsScroll={false} href="/" fontSize="16px" fontFamily="var(--font-primary), sans-serif" borderRadius='20px' borderSize='1.5px' opacity={1}></Btn>
+      <p style={{ marginBottom: 20 }}>Could not find requested content.</p>
+      <Btn
+        content="Return Home"
+        itsScroll={false}
+        href="/"
+        fontSize="16px"
+        fontFamily="var(--font-primary), sans-serif"
+        borderRadius="20px"
+        borderSize="1.5px"
+        opacity={1}
+      ></Btn>
     </div>
-  )
+  );
 }
--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -8,16 +8,56 @@ import Image from "next/image";
 // Context
 
 // Componenets
-import MenuDropdown from "../menu-dropdown/menu-dropdown.component";
+import MenuDropdown from "../r-menu-dropdown/menu-dropdown.component";
 
 const Header = () => {
+  const menuDropDown = [
+    {
+      "content": "Home",
+      "itsScroll": false,
+      "href": "/",
+      "sublinksId": 0,
+      "sublinks": []
+    },
+    {
+      "content": "Shop",
+      "itsScroll": false,
+      "href": "/shop",
+      "sublinksId": 0,
+      "sublinks": []
+    },
+    {
+      "content": "Categories",
+      "itsScroll": false,
+      "href": "",
+      "sublinksId": 1,
+      "sublinks": [
+        {
+          "content": "Kitchen",
+          "itsScroll": false,
+          "href": "/shop?cat=kitchen",
+        },
+        {
+          "content": "Bathroom",
+          "itsScroll": false,
+          "href": "/shop?cat=bathroom",
+        },
+        {
+          "content": "Livingroom",
+          "itsScroll": false,
+          "href": "/shop?cat=livingroom",
+        }
+      ]
+    }
+  ]
+
   const handleLogoClick = () => {
     window.location.href = '/'
   }
   return (
     <header className="article-header">
       <div className="container-left">
-        <MenuDropdown></MenuDropdown>
+        <MenuDropdown links={menuDropDown} fontSize="24px" fontFamily="var(--font-primary), sans-serif" borderSize="1px" />
       </div>
       <div className="container-logo">
         <Image className="logo" src={Logo} alt="logo" onClick={handleLogoClick} />
