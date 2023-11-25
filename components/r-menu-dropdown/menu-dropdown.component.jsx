a276bd5c28963ef2a0c2ccff2b2b91fa35736893
Adam <adambprograming@yahoo.com>
2023-11-25 13:45:42 +0100

    edit dropdown-menu with sublinks and other settings

--- components/r-menu-dropdown/menu-dropdown.component.jsx
+++ components/r-menu-dropdown/menu-dropdown.component.jsx
@@ -12,16 +12,16 @@ INSTRUCTIONS
     href          target of button onClick (if itsScroll===true, href must be id of target element)
     sublinksId    0 for link and 1/2/3 if link used to be for sublinks (first of it must have value of 1, second value of 2 and third value of 3)
     sublinks      list of sublinks with params same as link (content, itsScroll, href)
-  fontSize      fontSize in px (* multiplier)
-  fontFamily    fontFamily (could be like var(--font-primary), if fonts are set in variables)
+  fontSize      fontSize in px for mobile (it will be * by multiplier for desktop) (default set to 24px for mobile)
+  fontFamily    fontFamily (could be like var(--font-primary), if fonts are set in variables) (default set to var(--font-primary))
   borderSize    size of border (default set to 1px)
 */
 
-const MenuDropdown = ({ links, borderSize = "0px", fontSize, fontFamily }) => {
+const MenuDropdown = ({ links, borderSize = "0px", fontSize = "24px", fontFamily = "var(--font-primary)" }) => {
   const [activeMenu, setActiveMenu] = useState(false);
-  const [activeSubMenu1, setActiveSubMenu1] = useState(false)
-  const [activeSubMenu2, setActiveSubMenu2] = useState(false)
-  const [activeSubMenu3, setActiveSubMenu3] = useState(false)
+  const [activeSubMenu1, setActiveSubMenu1] = useState(false);
+  const [activeSubMenu2, setActiveSubMenu2] = useState(false);
+  const [activeSubMenu3, setActiveSubMenu3] = useState(false);
   const handleMenuClick = () => {
     document
       .getElementsByClassName("menu-icon")
@@ -39,35 +39,35 @@ const MenuDropdown = ({ links, borderSize = "0px", fontSize, fontFamily }) => {
       .getElementsByClassName("line-3")
       .item(0)
       .classList.remove("no-animation");
-    if(activeMenu === true){
-      handleDisactiveMenu()
+    if (activeMenu === true) {
+      handleDisactiveMenu();
     } else {
-      setActiveMenu(true)
+      setActiveMenu(true);
     }
   };
   const handleSubMenu = (index) => {
-    if(index === 1){
-      setActiveSubMenu1(!activeSubMenu1)
-      setActiveSubMenu2(false)
-      setActiveSubMenu3(false)
-    } else if (index === 2){
-      setActiveSubMenu2(!activeSubMenu2)
-      setActiveSubMenu1(false)
-      setActiveSubMenu3(false)
-    } else if (index === 3){
-      setActiveSubMenu3(!activeSubMenu3)
-      setActiveSubMenu1(false)
-      setActiveSubMenu2(false)
+    if (index === 1) {
+      setActiveSubMenu1(!activeSubMenu1);
+      setActiveSubMenu2(false);
+      setActiveSubMenu3(false);
+    } else if (index === 2) {
+      setActiveSubMenu2(!activeSubMenu2);
+      setActiveSubMenu1(false);
+      setActiveSubMenu3(false);
+    } else if (index === 3) {
+      setActiveSubMenu3(!activeSubMenu3);
+      setActiveSubMenu1(false);
+      setActiveSubMenu2(false);
     } else {
       console.error("Need more subMenus!");
     }
-  }
+  };
   const handleDisactiveMenu = () => {
-    setActiveMenu(false)
-    setActiveSubMenu1(false)
-    setActiveSubMenu2(false)
-    setActiveSubMenu3(false)
-  }
+    setActiveMenu(false);
+    setActiveSubMenu1(false);
+    setActiveSubMenu2(false);
+    setActiveSubMenu3(false);
+  };
   return (
     <nav className="nav">
       <div
@@ -82,47 +82,81 @@ const MenuDropdown = ({ links, borderSize = "0px", fontSize, fontFamily }) => {
       </div>
       <ul className={`dropdown ${activeMenu === true ? "active" : ""}`}>
         {links.map((link, index) => (
-          <li style={{ border: `${borderSize} solid var(--black-10)`, }} className="dropdown-item" key={index} >
+          <li
+            style={{ border: `${borderSize} solid var(--black-10)` }}
+            className="dropdown-item"
+            key={index}
+          >
             <button
               onClick={() => {
                 link.sublinksId !== 0
-                  ? (
-                    handleSubMenu(link.sublinksId)
-                  )
-                  : (link.itsScroll
-                  ? (document
+                  ? handleSubMenu(link.sublinksId)
+                  : link.itsScroll
+                  ? document
                       .getElementById(`#${link.href}`)
                       .scrollIntoView({ behavior: "smooth" }) &&
-                    handleDisactiveMenu())
-                  : (window.location.href = `${link.href}`));
+                    handleDisactiveMenu()
+                  : (window.location.href = `${link.href}`);
               }}
             >
-              <span style={{ fontFamily: fontFamily, fontSize: `calc(${fontSize} * var(--multiplier))` }} >{link.content}</span>
+              <span
+                className="dropdown-item-text"
+                style={{
+                  fontFamily: fontFamily,
+                  fontSize: `calc(${fontSize} * var(--multiplier))`,
+                }}
+              >
+                {link.content}
+              </span>
             </button>
-            {
-                link.sublinksId > 0 && (
-                  <ul style={{ top: `calc(${index}px * ${document.querySelector('.nav .dropdown .dropdown-item') ? document.querySelector('.nav .dropdown .dropdown-item').clientHeight : 0} + 5px + ${index} * ${borderSize} * 2)`}} className={`dropdown${link.sublinksId} ${(((link.sublinksId === 1 && activeSubMenu1) === true) || ((link.sublinksId === 2 && activeSubMenu2) === true) || ((link.sublinksId === 3 && activeSubMenu3) === true)) ? ("active") : ("")}`}>
-                    {
-                      link.sublinks.map((sublink, index) => (
-                        <li style={{ border: `${borderSize} solid var(--black-10)`, }} className="dropdown-item" key={index}>
-                          <button
-                            onClick={() => {
-                              (link.itsScroll
-                                ? (document
-                                    .getElementById(`#${link.href}`)
-                                    .scrollIntoView({ behavior: "smooth" }) &&
-                                  handleDisactiveMenu())
-                                : (window.location.href = `${link.href}`));
-                            }}
-                          >
-                            <span style={{ fontFamily: fontFamily, fontSize: `calc(${fontSize} * var(--multiplier))` }} >{sublink.content}</span>
-                          </button>
-                        </li>
-                      ))
-                    }
-                  </ul>
-                ) 
-              }
+            {link.sublinksId > 0 && (
+              <ul
+                style={{
+                  top: `calc(${index}px * ${
+                    document.querySelector(".nav .dropdown .dropdown-item")
+                      ? document.querySelector(".nav .dropdown .dropdown-item")
+                          .clientHeight
+                      : 0
+                  } + 5px + ${index} * ${borderSize} * 2)`,
+                }}
+                className={`dropdown${link.sublinksId} ${
+                  (link.sublinksId === 1 && activeSubMenu1) === true ||
+                  (link.sublinksId === 2 && activeSubMenu2) === true ||
+                  (link.sublinksId === 3 && activeSubMenu3) === true
+                    ? "active"
+                    : ""
+                }`}
+              >
+                {link.sublinks.map((sublink, index) => (
+                  <li
+                    style={{ border: `${borderSize} solid var(--black-10)` }}
+                    className="dropdown-item-sublink"
+                    key={index}
+                  >
+                    <button
+                      onClick={() => {
+                        link.itsScroll
+                          ? document
+                              .getElementById(`#${link.href}`)
+                              .scrollIntoView({ behavior: "smooth" }) &&
+                            handleDisactiveMenu()
+                          : (window.location.href = `${link.href}`);
+                      }}
+                    >
+                      <span
+                        className="dropdown-item-sublink-text"
+                        style={{
+                          fontFamily: fontFamily,
+                          fontSize: `calc(${fontSize} * var(--multiplier))`,
+                        }}
+                      >
+                        {sublink.content}
+                      </span>
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
